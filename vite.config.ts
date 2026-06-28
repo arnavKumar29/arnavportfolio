7d470dac26940183bb030401b3b2516ce8eca6f7
arnavKumar29 <arnar.kumar29@gmail.com>
2026-06-28 09:32:54 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/arnavportfolio/",
 
   plugins: [react(), tailwindcss(), viteSingleFile()],
 
